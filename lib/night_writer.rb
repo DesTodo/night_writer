df431482c12793b8c26ce258269663f266b57669
Desislava Todorova <dslavatodorova@gmail.com>
2017-05-14 22:51:17 -0600

    Initial night_writer files

---
+++ lib/night_writer.rb
@@ -0,0 +1,3 @@
+class NightWriterTest
+
+end
---
+++ test/night_writer_test.rb
@@ -0,0 +1,17 @@
+def test_if_file_exists
+file = File.open(incoming_test), "r"]
+
+# get first word from incoming_test
+
+# incoming_test.class ExpectedClass
+
+
+end test_if_print_times_three
+# input a line from file
+incoming_test[1]
+
+# print each onput times three
+def
+
+
+letter_a = {:a => "0./n00/n.."}
