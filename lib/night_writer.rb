e24d57690d5bb13c4613801d9ff9559114e58a74
Desislava Todorova <dslavatodorova@gmail.com>
2017-05-14 23:12:41 -0600

    Second Commit

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,76 @@
-class NightWriterTest
+# which are all the classes /objects needed
+
+class FileReader
+  def read
+    filename = ARGV[0]
+    File.read(filename)
+  end
+end
+
+class NightWriter
+  attr_reader :file_reader
+
+  def initialize
+    @reader = FileReader.new
+  end
+
+  def encode_file_to_braille
+    # I wouldn't worry about testing this method
+    # unless you get everything else done
+    plain = reader.read
+    braille = encode_to_braille(plain)
+  end
+
+  def encode_to_braille(input)
+    # you've taken in an INPUT string
+    # do the magic
+    # send out an OUTPUT string
+  end
+end
+
+class BrailleCharacter
+  attr_reader :dots, :print
+
+
+  def initialize(dots)
+    @dots = dots
+  end
+
+  def print
+    dots.each do |dot|
+      p dot.join
+    end
+  end
+
+
+end
+
+class BrailleRow
+    attr_reader :braille_characters
+
+    def initialize(row)
+      @braille_characters = []
+
+      input_length = row[0].length
+      index = 0
+
+      while index < input_length
+
+        @braille_characters << BrailleCharacter.new(
+          [
+            [row[0][index], row[0][index + 1]],
+            [row[1][index], row[1][index + 1]],
+            [row[2][index], row[2][index + 1]]
+          ]
+        )
+        index += 2
+
+      end
+
+    end
+
+    def print
+
+    end
 
 end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,17 +1,49 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/braile_character'
+require './lib/braile_row'
+require './lib/night_writer'
+
+class TextFileTest <Minitest::Test
+
 def test_if_file_exists
 file = File.open(incoming_test), "r"]
-
 # get first word from incoming_test
-
 # incoming_test.class ExpectedClass
+end
 
 
-end test_if_print_times_three
+def test_if_print_times_three
 # input a line from file
 incoming_test[1]
-
 # print each onput times three
-def
+end
 
+letter_a = {:a => [["0","."], [".", "."], [".","."]]}
 
-letter_a = {:a => "0./n00/n.."}
+class BrailleCharacterTest < Minitest::Test
+
+  def test_it_exists
+    braille_a = BrailleCharacter.new([["0", "."], [".","."], [".","."]])
+    assert_equal BrailleCharacter, braile_a.class
+    #assert_match
+    braille_a.dots.include?(["0", "."]), ([["0", "."], [".","."], [".","."]])
+  end
+
+end
+
+class BrailleRowTest < Minitest::Test
+
+  def test_it_prints_in_three_lines_per_row_of_braille
+
+    line_one = "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.".chars
+    line_two = "...........Y....................................................................".chars
+    line_three = "..........X.....................................................................".chars
+    row_of_braille = [line_one, line_two, line_three]
+    brailleRow = BrailleRow.new(row_of_braile)
+    brailleRow.braille_characters[5].print
+    true
+  end
+
+end
