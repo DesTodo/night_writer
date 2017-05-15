79fda83c394477078366ef5b3584cc3861eeebb1
Desislava Todorova <dslavatodorova@gmail.com>
2017-05-15 13:11:49 -0600

    t exit :w :q

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,22 +1,40 @@
-# which are all the classes /objects needed
+# input_file_name = ARGV[0]
+# output_file_name = ARGV[1]
 
 class FileReader
   def read
     filename = ARGV[0]
-    File.read(filename)
+    File.read(filename).split("\n")
+  end
+end
+
+class FileWriter
+  def write(output)
+    filename = ARGV[1]
+    file_writer = File.open(filename, "w")
+    file_writer.write(output)
+  end
+end
+
+class BraileLibrary
+  attr_reader :braille_to_english,
+        :english_to_braille
+
+  def initialize
+
   end
 end
 
 class NightWriter
-  attr_reader :file_reader
+  attr_reader :reader,
+          :writer
 
   def initialize
     @reader = FileReader.new
+    @writer = FileWriter.new
   end
 
   def encode_file_to_braille
-    # I wouldn't worry about testing this method
-    # unless you get everything else done
     plain = reader.read
     braille = encode_to_braille(plain)
   end
@@ -25,52 +43,11 @@ class NightWriter
     # you've taken in an INPUT string
     # do the magic
     # send out an OUTPUT string
+
+    writer.write(input)
+
   end
 end
 
-class BrailleCharacter
-  attr_reader :dots, :print
-
-
-  def initialize(dots)
-    @dots = dots
-  end
-
-  def print
-    dots.each do |dot|
-      p dot.join
-    end
-  end
-
-
-end
-
-class BrailleRow
-    attr_reader :braille_characters
-
-    def initialize(row)
-      @braille_characters = []
-
-      input_length = row[0].length
-      index = 0
-
-      while index < input_length
-
-        @braille_characters << BrailleCharacter.new(
-          [
-            [row[0][index], row[0][index + 1]],
-            [row[1][index], row[1][index + 1]],
-            [row[2][index], row[2][index + 1]]
-          ]
-        )
-        index += 2
-
-      end
-
-    end
-
-    def print
-
-    end
-
-end
+night_writer = NightWriter.new
+night_writer.encode_file_to_braille
